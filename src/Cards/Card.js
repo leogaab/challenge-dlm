2b99dca816b39501a403119d9abe8bfa8b22acfa
leogaab <leonelgaab@gmail.com>
2022-10-03 20:20:37 -0300

    Fixed Cards

--- src/Cards/Card.js
+++ src/Cards/Card.js
@@ -7,22 +7,25 @@ const Card = () => {
 
   useEffect(() =>  {
 
-    fetch('https://jsonplaceholder.typicode.com/todos')
+    fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json()
       .then(resp => setResults(resp) )
     )
-  }, [])
+  }, [results])
   
   console.log('results: ', results)
   return (
     <Container>
-      <ul>
-          {results && results.map( result => <li>{result.id}</li>)}
-      </ul>
+      <CardContainer>
+          {results && results.map( result => (
+            <Item key={result.id}>
+              <ItemTitle>{result.title}</ItemTitle>
+              <ItemDescription>{result.body}</ItemDescription>
+            </Item>
+          ))}
+      </CardContainer>
     </Container>
   )
-
-
 }
 
 export default Card;
@@ -31,3 +34,29 @@ const Container = styled.div`
   border: 2px solid red;
   margin-left: 12vw;
 `
+
+const CardContainer = styled.ul`
+  list-style-type: none;
+  border: 2px solid blue;
+  display: flex;
+  flex-wrap: wrap;
+`
+
+const Item = styled.li`
+  border: 2px solid green;
+  border-radius: 4px;
+  padding: 20px;
+  margin: 10px auto;
+  max-width: 25vw;
+  box-sizing: border-box;
+  display: inline-block;
+  overflow: hidden;
+`
+
+const ItemTitle = styled.h2`
+  margin-top: 0;
+  color: #2c3e50;
+  line-break: strict;
+`
+
+const ItemDescription = styled.p``
