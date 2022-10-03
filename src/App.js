c34a31e9f70029b9825b83f5629c4b57a84d9130
leogaab <leonelgaab@gmail.com>
2022-10-03 20:19:44 -0300

    Router added and forced state

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import { useState } from 'react';
 import Card from './Cards/Card';
 import LogIn from './Login/Login';
 import Menu from './Menu/Menu';
+import Home from './Home/Home';
+import Search from './Search/Search';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ErrorPage from './ErrorPage';
 
 function App() {
-
   const [isLoggedIn, setIsLoggedIn] = useState(false)
-  
   return (
     <>
-      {!isLoggedIn ? (<LogIn onClick={()=> setIsLoggedIn(true)} />)
-      : (
-        <>
-          <Menu isLoggedIn={isLoggedIn} onClick={()=> {setIsLoggedIn(!isLoggedIn)}} />
-          <Card />
-        </>
-      )
-      }
+    {!isLoggedIn ? (<LogIn onClick={()=> setIsLoggedIn(true)} />) :
+    (<Router>
+      <Menu isLoggedIn={isLoggedIn} onClick={()=> {setIsLoggedIn(!isLoggedIn)}} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cards" element={<Card />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>)}
     </>
+        
   );
 }
 
